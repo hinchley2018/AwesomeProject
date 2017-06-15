64db0d97512fc45e9ed8d6d83ab6057b8e6d3efe
jhinchley <dev.jjh@planet-hinchley.com>
2017-06-15 18:24:51 -0500

    fetch movies

--- index.android.js
+++ index.android.js
@@ -7,46 +7,92 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+var MOCKED_MOVIES_DATA = [
+    {title: 'Title', year:'2015',posters:{thumbnail:'http://i.imgur.com/UePbdph.jpg'}}
+];
+
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 export default class AwesomeProject extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      movies:null,
+    };
+  }
+
   render() {
+    var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <Image
+            source={{uri: movie.posters.thumbnail}}
+            style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>
+              {movie.title}
+          </Text>
+          <Text style={styles.year}>
+              {movie.year}
+          </Text>
+
+        </View>
+
+
       </View>
     );
   }
+
+  componentDidMount(){
+    this.fetchData();
+  }
+
+    fetchData() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+              this.setState({
+                  movies: responseData.movies,
+              });
+            })
+            .done();
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  rightContainer: {
+      flex: 1,
   },
-  instructions: {
+
+  title: {
+    fontSize:20,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    marginBottom: 8,
+  },
+  year: {
+      textAlign: 'center',
+
+
+  },
+  thumbnail: {
+    width: 80,
+    height: 80,
   },
 });
 
